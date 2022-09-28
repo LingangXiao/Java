a5ca96da1c435258ea2e3622d638ed097b3a03e9
LingangXiao <89891807+LingangXiao@users.noreply.github.com>
2022-09-28 01:10:12 -0500

    Add files via upload

---
+++ javaheima/src/methods/methods.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class methods {
+    public static void main(String[] args) {
+        int d = sum(10,30);
+        System.out.println(d);
+    }
+
+    public static int sum (int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+    
+
+}
